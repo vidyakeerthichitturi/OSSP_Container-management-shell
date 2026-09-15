149107f605055e3bf4dcbcc4868330a601d73479
Vidya Keerthi Chitturi <vidyakeerthi.chitturi@klh.edu.in>
2026-09-15 17:46:13 +0000

    Week 3: Added command parser using strtok

--- src/main.c
+++ src/main.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "../include/shell.h"
 #include "../include/input.h"
+#include "../include/parser.h"
 
 int main()
 {
     char *line;
-    printf("=================================\n");
-    printf("%s Version %s\n",SHELL_NAME,VERSION);
-    printf("=================================\n");
+    char **tokens;
+    int i;
 
-    while(1)
+    printf("=====================================\n");
+    printf("ShellForge Version 3.0\n");
+    printf("=====================================\n");
+
+    while (1)
     {
         printf("myshell> ");
+
         line = read_line();
-        if(strcmp(line,"exit")==0)
+
+        if (strcmp(line, "exit") == 0)
         {
             free(line);
             break;
         }
-        if(strlen(line)!=0)
-            printf("You entered : %s\n",line);
+
+        tokens = parse_line(line);
+
+        printf("\nParsed Tokens\n");
+
+        for (i = 0; tokens[i] != NULL; i++)
+        {
+            printf("argv[%d] = %s\n", i, tokens[i]);
+        }
+
+        free_tokens(tokens);
         free(line);
     }
+
     printf("Goodbye!\n");
+
     return 0;
 }
